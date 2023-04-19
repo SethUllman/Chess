2c6f33fd619c3dab59112ebb295ad2e796ea15f7
Seth Ullman <seth.ullman98@gmail.com>
2023-04-18 22:33:41 -0600

    Add empty Piece methods

--- Piece.java
+++ Piece.java
@@ -1,2 +1,24 @@
 public class Piece {
+    final int White = 0;
+    final int Black = 1;
+
+    int color;
+    int[][] currentPosition;
+    int[][] availableMoves;
+
+    public int[][] getPosition(){
+        return new int[1][1];
+    }
+
+    public void setPosition(int[][] pos){
+
+    }
+
+    public void updateMoves(){
+
+    }
+
+    public void move(){
+
+    }
 }
