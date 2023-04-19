86087e42d217144c5652a3961550c3af02671e6b
Seth Ullman <seth.ullman98@gmail.com>
2023-04-18 22:28:51 -0600

    Add empty Game methods

--- Game.java
+++ Game.java
@@ -1,2 +1,28 @@
 public class Game {
+
+    final int White = 0;
+    final int Black = 1;
+    int turn = White;
+    Board board = new Board();
+
+    public void start(){
+
+    }
+
+    public void makeMove(int[][] start, int[][] end){
+
+    }
+
+    public boolean validMove(int[][] start, int[][] end){
+        return true;
+    }
+
+    public boolean gameOver(){
+        return false;
+    }
+
+    public boolean checkMate(){
+        return false;
+    }
+
 }
